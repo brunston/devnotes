2124a9b3a7e021a84e5d7d42f0e9d287384eb267
Brunston Poon <albatrossian@outlook.com>
2016-05-03 13:31:46 -0400

    proper initalizatiion, streamlined sql command execution

--- note.py
+++ note.py
@@ -1,12 +1,34 @@
 # Notes and Todo Software
+# main class file for note operation
 # brupoon
 
+import sqlite3
+ 
+def execute_sql(sql_command):
+        
+    conn = sqlite3.connect("note.db")
+    cursor = conn.cursor()
+    cursor.execute(sql_command)
+    conn.commit()
+    conn.close()
+
+def initalize_db():
+     
+    sql_ = """
+        CREATE TABLE note (
+        number INTEGER PRIMARY KEY,
+        date INTEGER,
+        text VARCHAR(255),
+        done INTEGER);"""
+    execute_sql(sql_)
+   
 class Note():
-    
+
     def __init__(self, date, text):
+        
         self.date = date
         self.text = text
-
+        
         if self.date == -1:
             self.todo = True
             self.appt = False
@@ -14,5 +36,19 @@ class Note():
             self.todo = False
             self.appt = True
 
-    def store(self):
+    def store_note(self):
         
+        sql_ = """
+        INSERT into note (date, text, done) VALUES (NULL, "{0}", "{1}", 0);
+        """.format(self.date, self.text)
+        execute_sql(sql_)
+
+    def get_all_notes(self):
+
+        sql_ = """
+        SELECT * FROM note WHERE done = 0"""
+        execute_sql(sql_)
+
+    def remove_note(self, number):
+       #for next time
+       pass
--- test.py
+++ test.py
@@ -2,6 +2,17 @@
 # brupoon
 
 import note
+import sqlite3
+db = note.initialize_db()
 
 dated = note.Note('20160530','appointment')
+dated.store_note()
 undated = note.Note('-1','todo')
+undated.store_note()
+conn = sqlite3.connect("note.db")
+cursor = conn.cursor()
+cursor.execute("SELECT * FROM note")
+
+print("fetchall:")
+for r in cursor.fetchall():
+    print(r)
